06cb9277be1f52f68aca4a7f5269c21988978884
Kolynes <kolyneschinedu@gmail.com>
2019-10-11 00:04:31 +0100

    set for deployment

--- CtrlX/settings.py
+++ CtrlX/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i#hbhh1c^4$pz&+-h1xnb9!=kt^=id7*xem@&buct^4ewflvok'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'CtrlX.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-if DEBUG:
+if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'mysql.connector.django',
